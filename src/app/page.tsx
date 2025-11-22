879d68cc268ff836f17448b5e221c17d51b0974b
sakin111 <maliksakin53@gmail.com>
2025-11-22 14:48:40 +0600

    created health app

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+
+import { Button } from "../components/ui/button";
+
+export default function Home() {
+  return (
+<div>
+  <h1>welcome to the healthcare</h1>
+  <Button variant="secondary">click me</Button>
+</div>
+  );
+}
